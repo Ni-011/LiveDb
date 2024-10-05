c0b48824309ebc28afd639cd88ed0caceb24556b
nitin <nitinrana01125532553@gmail.com>
2024-10-05 23:58:29 +0530

    modified Insert and make select feature

--- LiveDb/LiveDb.go
+++ LiveDb/LiveDb.go
@@ -39,58 +39,60 @@ func NewLiveDB() (*LiveDb, error) {
 
 // creating collecions,
 func (ldb LiveDb) CreateCollection(name string) (*collection, error) {
-	collection := collection{} // collection to store the user created bucket
-	// create a read-write bucket with user sent name
-	err := ldb.DB.Update(func(tx *bbolt.Tx) error {
-		var (
-			err    error
-			bucket *bbolt.Bucket
-		)
-		bucket = tx.Bucket([]byte(name)) // search for the bucket
-		if bucket == nil {               // if bucket doesn't exist, make it
-			bucket, err = tx.CreateBucket([]byte(name))
-			if err != nil {
-				return err
-			}
-		}
-		collection.Bucket = bucket // update collection with the bucket found/made
-		return nil
-	})
+	tx, err := ldb.DB.Begin(true) // read/write method
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback() // defer the commit
 
+	Bucket, err := tx.CreateBucketIfNotExists([]byte(name)) // find or create the bucket
 	if err != nil {
 		return nil, err
 	}
 
-	return &collection, nil // return the collection
+	return &collection{Bucket}, nil
 }
 
 func (ldb *LiveDb) Insert(collectionName string, data Data) (uuid.UUID, error) {
 	id := uuid.New()
 
-	// get/make the collection user wants
-	collection, err := ldb.CreateCollection(collectionName)
+	tx, err := ldb.DB.Begin(true) // read/write method
+	if err != nil {
+		return id, err
+	}
+	defer tx.Rollback() // defer the commit
+
+	Bucket, err := tx.CreateBucketIfNotExists([]byte(collectionName)) // find or create the bucket
 	if err != nil {
 		return id, err
 	}
 
-	// inserting user's data into the collection
-	ldb.DB.Update(func(tx *bbolt.Tx) error { // read/write method
-		for key, value := range data {
-			if err := collection.Put([]byte(key), []byte(value)); err != nil {
-				return err
-			}
+	// inserting user's data into the bucket 
+	for key, value := range data {
+		if err := Bucket.Put([]byte(key), []byte(value)); err != nil {
+			return id, err
 		}
+	}
 
-		// putting id into the collection
-		if err := collection.Put([]byte("id"), []byte(id.String())); err != nil {
-			return err
-		}
-		return nil
-	})
+	// putting id into the collection
+	if err := Bucket.Put([]byte("id"), []byte(id.String())); err != nil {
+		return id, err
+	}
 
-	return id, nil
+	return id, tx.Commit()
 }
 
-func (lbd *LiveDb) Select (collection string, key string, query any) {
 
+// return bucket data using a query, read only
+func (lbd *LiveDb) Select(collection string,  query Data) (Data, error) {
+	tx, err := lbd.DB.Begin(true) // read/write method
+	if err != nil {
+		return nil, err
+	}
+
+	bucket := tx.Bucket([]byte(collection)) // find user requested bucket
+	if bucket == nil {
+		// bucket must exist
+		return nil, fmt.Errorf("collection %s not found", collection)
+	}
 }
